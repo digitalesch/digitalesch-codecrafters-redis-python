d4ffae8643c2afba3577705b7efb4d51a192fe70
Felipe Drechsler <felipe.eduardo.dl@gmail.com>
2025-10-09 14:22:03 -0300

    sending pong

--- app/main.py
+++ app/main.py
@@ -6,10 +6,7 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-
     # accept connections
     connection, _ = server_socket.accept()
     connection.sendall(b"+PONG\r\n")
