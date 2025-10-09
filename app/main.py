4f341184cff43d80a97d84c04da414271fdf4023
Felipe Drechsler <felipe.eduardo.dl@gmail.com>
2025-10-09 14:18:35 -0300

    sending pong

--- app/main.py
+++ app/main.py
@@ -10,6 +10,10 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
 
+    # accept connections
+    connection, _ = server_socket.accept()
+    connection.sendall(b"+PONG\r\n")
+
 
 if __name__ == "__main__":
     main()
